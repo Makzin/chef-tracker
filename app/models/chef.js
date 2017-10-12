089e95d8c07efa399473697a9914c6bbf5bf3fd4
Max Van Lyl <maxim@freshbooks.com>
2017-10-12 16:05:03 -0400

    added nested routes and multi-pane view

--- app/models/chef.js
+++ app/models/chef.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   isCooking: DS.attr('boolean'),
-  numberOfStudents: DS.attr('number', {defaultValue: 0})
+  numberOfStudents: DS.attr('number', {defaultValue: 0}),
+  description: DS.attr('string')
 });
